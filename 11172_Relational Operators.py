a72f9d84da8bdeb500147cea4c9a8a2d02434814
mohamedkhaledd <mohamedte7a1997kh@gmail.com>
2019-02-04 03:53:05 +0200

    Create 11172_Relational Operators.py

---
+++ 11172_Relational Operators.py
@@ -0,0 +1,12 @@
+tc = int(input())
+
+for i in range(tc):
+    a,b = map(int , input().split())
+    if(a > b):
+        print('>')
+    elif(a < b):
+        print('<')
+    else:
+        print('=')
+
+
